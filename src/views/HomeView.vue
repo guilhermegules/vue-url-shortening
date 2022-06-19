4df017bc8c37b0a318787a923979499f2aa8448a
Guilherme Gules <guilhermegules@gmail.com>
2022-06-19 13:28:09 -0300

    feat: update the home structure

---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <NavComponent />
+    <HeaderComponent />
+    <ShorteningUrlFormComponent />
+    <InfoComponent />
+    <FooterComponent />
+  </main>
+</template>
+
+<script lang="ts">
+import HeaderComponent from "../components/HeaderComponent.vue";
+import NavComponent from "../components/NavComponent.vue";
+import FooterComponent from "../components/FooterComponent.vue";
+import InfoComponent from "../components/home-components/InfoComponent.vue";
+import ShorteningUrlFormComponent from "../components/home-components/ShorteningUrlFormComponent.vue";
+
+export default {
+  components: {
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+    InfoComponent,
+    ShorteningUrlFormComponent,
+  },
+};
+</script>
