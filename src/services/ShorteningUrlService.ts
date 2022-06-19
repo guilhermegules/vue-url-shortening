f4a0620cd6c81c6538962d89ac39a5a8ff9771d2
Guilherme Gules <guilhermegules@gmail.com>
2022-06-19 13:25:50 -0300

    feat: creating url shortening service

---
+++ src/services/ShorteningUrlService.ts
@@ -0,0 +1,11 @@
+import type { ShortenUrlResponse } from "@/models/ShorteningServiceModel";
+
+const API_URL = "https://api.shrtco.de/v2/";
+
+export default {
+  async shorteningUrl(url: string) {
+    const response = await fetch(`${API_URL}/shorten?url=${url}`);
+    const shortenUrl: ShortenUrlResponse = await response.json();
+    return shortenUrl;
+  },
+};
