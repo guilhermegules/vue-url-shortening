5c82c20bff7c41c34ae01d45301248e3a62b7e67
Guilherme Gules <guilhermegules@gmail.com>
2022-06-26 22:05:52 -0300

    feat: creating info section

---
+++ src/components/home-components/InfoCardComponent.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="card">
+    <span class="card__image-container">
+      <img :src="icon" :alt="title" />
+    </span>
+    <h3 class="card__title">{{ title }}</h3>
+    <p class="card__text">
+      {{ text }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    title: String,
+    text: String,
+    icon: String,
+  },
+};
+</script>
+
+<style scoped>
+.card {
+  width: 300px;
+  height: 300px;
+  position: relative;
+  background-color: var(--neutral-white);
+  padding: 80px 20px 30px 20px;
+  text-align: left;
+  border-radius: 8px;
+}
+
+.card__image-container {
+  width: 80px;
+  background-color: var(--primary-dark-violet);
+  position: absolute;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 80px;
+  top: -30px;
+}
+
+.card__image-container img {
+  width: 40px;
+  height: 40px;
+}
+
+.card__title {
+  margin-bottom: 15px;
+  color: var(--primary-dark-violet);
+}
+
+.card__text {
+  color: var(--neutral-grayish-violet);
+}
+</style>
--- src/components/home-components/InfoComponent.vue
+++ src/components/home-components/InfoComponent.vue
@@ -1,3 +1,84 @@
 <template>
-  <p>Info component</p>
+  <section class="info">
+    <h2 class="info__title">Advanced Statistics</h2>
+    <p class="info__text">
+      Track how your links are performing across the web with our advanced
+      statistics dash board
+    </p>
+
+    <div class="info__cards">
+      <InfoCard
+        title="Brand Recognition"
+        text="Boost your brand recognition with each click. Generic links help instil
+      confidence in your content."
+        icon="/images/icon-brand-recognition.svg"
+      />
+      <InfoCard
+        title="Detailed Records"
+        text="Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions."
+        icon="/images/icon-detailed-records.svg"
+      />
+      <InfoCard
+        title="Fully Customizable"
+        text="Improve brand awareness and content discoverability through customizable links, supercharging audience engagement."
+        icon="/images/icon-fully-customizable.svg"
+      />
+    </div>
+
+    <div class="info__boost">
+      <h2>Boost your links today</h2>
+      <ButtonComponent text="Get Started" rounded withBackground big />
+    </div>
+  </section>
 </template>
+
+<script lang="ts">
+import InfoCard from "./InfoCardComponent.vue";
+import ButtonComponent from "../ButtonComponent.vue";
+
+export default {
+  components: {
+    InfoCard,
+    ButtonComponent,
+  },
+};
+</script>
+
+<style scoped>
+.info {
+  text-align: center;
+  padding: 200px 50px 0;
+  background-color: var(--neutral-light-gray);
+  margin: 0 -50px;
+}
+
+.info__title {
+  color: var(--primary-dark-violet);
+}
+
+.info__text {
+  color: var(--neutral-grayish-violet);
+  margin-bottom: 50px;
+}
+
+.info__cards {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.info__boost {
+  padding: 30px 50px;
+  margin: 100px -50px 0;
+  background-image: url(/images/bg-shorten-desktop.svg);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  background-color: var(--primary-dark-violet);
+}
+
+.info__boost h2 {
+  color: var(--neutral-white);
+  margin-bottom: 20px;
+}
+</style>
