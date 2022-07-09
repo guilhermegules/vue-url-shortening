c0cf3320fa31f47aa156ca5d8c9ae35681469e4c
Guilherme Gules <guilhermegules@gmail.com>
2022-07-08 22:01:21 -0300

    feat: creating mobile friend menu

--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -8,6 +8,24 @@
         <a href="">Pricing</a>
         <a href="">Resources</a>
       </div>
+
+      <img
+        class="nav__menu-hamburger"
+        src="images/menu.svg"
+        alt="menu icon"
+        @click="showMenu()"
+      />
+
+      <div class="nav__menu-mobile" v-bind:class="{ 'fade-out': isVisible }">
+        <div>
+          <a href="">Features</a>
+          <a href="">Pricing</a>
+          <a href="">Resources</a>
+        </div>
+
+        <ButtonComponent text="Login" />
+        <ButtonComponent withBackground rounded text="Sign Up" />
+      </div>
     </div>
 
     <div class="nav__buttons">
@@ -18,13 +36,24 @@
 </template>
 
 <script lang="ts">
+import { defineComponent } from "vue";
 import ButtonComponent from "./ButtonComponent.vue";
 
-export default {
+export default defineComponent({
   components: {
     ButtonComponent,
   },
-};
+  data() {
+    return {
+      isVisible: false,
+    };
+  },
+  methods: {
+    showMenu() {
+      this.isVisible = !this.isVisible;
+    },
+  },
+});
 </script>
 
 <style scoped>
@@ -34,6 +63,10 @@ export default {
   align-items: center;
   width: 100%;
   padding: 30px 0;
+}
+
+.nav__menu-hamburger {
+  display: none;
 }
 
 .nav__links {
@@ -49,6 +82,15 @@ export default {
   justify-content: space-around;
 }
 
+.nav__menu-mobile {
+  display: none;
+}
+
+.fade-out {
+  opacity: 0;
+  transition: opacity 500ms linear;
+}
+
 .nav__sub-links a {
   color: var(--neutral-grayish-violet);
   text-decoration: none;
@@ -60,5 +102,59 @@ export default {
 
 .nav__buttons > button:first-of-type {
   margin-right: 15px;
+}
+
+@media screen and (max-width: 512px) {
+  .nav__links {
+    position: relative;
+    width: 100%;
+  }
+
+  .nav__sub-links {
+    display: none;
+  }
+
+  .nav__buttons {
+    display: none;
+  }
+
+  .nav__menu-hamburger {
+    display: block;
+  }
+
+  .nav__menu-mobile {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: var(--primary-dark-violet);
+    width: 100%;
+    position: absolute;
+    z-index: 2;
+    color: var(--neutral-white);
+    top: 65px;
+    padding: 30px;
+    border-radius: 15px;
+    transition: opacity 500ms linear;
+  }
+
+  .nav__menu-mobile button {
+    width: 100%;
+  }
+
+  .nav__menu-mobile div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    gap: 15px;
+    padding-bottom: 15px;
+    border-bottom: 1px solid var(--neutral-grayish-violet);
+  }
+
+  .nav__menu-mobile div a {
+    color: var(--neutral-white);
+    text-decoration: none;
+    font-weight: bold;
+  }
 }
 </style>
